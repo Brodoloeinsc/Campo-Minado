dc1241fe307d256f63169b4744617f772855fa11
Brodoloeins <joaofernandovascao@gmail.com>
2024-02-25 09:19:43 -0300

    Dinamizando o campo

--- main.c
+++ main.c
@@ -6,12 +6,22 @@ int main(){
     // 1 ponto com bomba
     // 2 ponto marcado préviamente
 
-    int campo[9][9], bombas[9][2];
-    
-    bombas[0][0] = rand() % (9 + 1 - 0) + 0;
-    bombas[0][1] = rand() % (9 + 1 - 0) + 0;
+    int j, i, campo[9][9], bombas[9][2];
 
-    campo[bombas[0][0]][bombas[0][1]] = 1;
+    for(i = 0; i < 9; i++){
+        for(j = 0; j < 9; j++){
+            campo[i][j] = 0;
+        }
+    }
+
+    for(i = 0; i < 9; i++){
+        bombas[i][0] = rand() % (9 + 1 - 0) + 0;
+        bombas[i][1] = rand() % (9 + 1 - 0) + 0;
+
+        campo[bombas[i][0]][bombas[i][1]] = 1;
+    }
+
+    
 
     return 0;
 }
